dc6d5810c7fd4e7d6d8c379010977090008421e4
Aditto dev Barmon (koushik) <134207015+Adityo-dev@users.noreply.github.com>
2025-11-05 06:38:53 +0600

    Shop Page Search Input Add

--- src/components/shop/sidebarFilters/CheckboxFilter.js
+++ src/components/shop/sidebarFilters/CheckboxFilter.js
@@ -1,4 +1,5 @@
 import Checkbox from "@/components/ui/Checkbox";
+import { useState } from "react";
 
 function CheckboxFilter({
   title,
@@ -8,6 +9,8 @@ function CheckboxFilter({
   getLabel, // optional
   className = "",
 }) {
+  const [search, SetSearch] = useState("");
+
   const handleChange = (item, checked) => {
     const updated = checked
       ? [...selectedValues, item]
@@ -20,12 +23,32 @@ function CheckboxFilter({
     return typeof getLabel === "function" ? getLabel(item) : item;
   };
 
+  //   filtered Options
+  const filteredOptions = options.filter((item) =>
+    getItemLabel(item).toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className={className}>
+      {/* title */}
       {title && <h3 className="text-lg mb-2 mt-4">{title}</h3>}
 
+      {/* search box */}
+      <input
+        type="text"
+        placeholder="Search..."
+        value={search}
+        onChange={(e) => SetSearch(e.target.value)}
+        className="
+            w-full px-3 py-2 text-sm rounded-md border border-gray-600 
+            bg-transparent focus:outline-none focus:ring-2 focus:ring-blue-500
+            mb-3
+          "
+      />
+
+      {/* data show */}
       <div className="w-full max-h-[300px] h-full overflow-y-auto custom-scrollbar">
-        {options.map((item) => (
+        {filteredOptions.map((item) => (
           <Checkbox
             key={item}
             id={String(item)}
