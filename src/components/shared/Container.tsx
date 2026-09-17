6796e783950028b1fa75d7b75918729fc963bff5
Aditto dev Barmon (koushik) <134207015+Adityo-dev@users.noreply.github.com>
2026-09-18 04:53:22 +0800

    Update max-w in Container.tsx

--- src/components/shared/Container.tsx
+++ src/components/shared/Container.tsx
@@ -7,7 +7,7 @@ export interface ContainerProps {
 
 export default function Container({ children, className = "" }: ContainerProps): React.JSX.Element {
   return (
-    <div className={`container mx-auto px-2.5 sm:px-4 ${className}`}>
+    <div className={`max-w-[1920px] mx-auto px-2.5 sm:px-4 ${className}`}>
       {children}
     </div>
   );
